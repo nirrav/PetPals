3e60cbd7d50206dc3a658c52d87ecb2392d0be92
Nirrav Sawla <nirravsawla@gmail.com>
2024-04-27 21:01:48 +0530

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+  
